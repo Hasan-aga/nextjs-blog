06098854528be4ed97407715b7aaccba1767faf6
Hasan-aga <careerforma@gmail.com>
2022-11-19 16:34:17 +0300

    SSG with data

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../utils/processPost";
 
-export default function Home() {
+export default function Home({ posts }) {
+  console.log({ posts });
   return (
     <Layout home>
       <Head>
@@ -14,7 +16,23 @@ export default function Home() {
           (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+        {posts.map(({ id, date, title }) => (
+          <li className={utilStyles.listItem} key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const posts = getSortedPostsData();
+  return {
+    props: { posts }, // will be passed to the page component as props
+  };
 }
---
+++ posts/first_article.md
@@ -0,0 +1,8 @@
+---
+title: Hello markdown
+date: 19-11-2022
+---
+
+## hello
+
+this is _markdown_ and I like `it`.
